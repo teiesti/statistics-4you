f9c4c47b802e9f9d13ca0575b1b022fa15874ebd
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2025-08-25 17:56:57 +0200

    Setup logging and error handling

---
+++ build.rs
@@ -0,0 +1,10 @@
+use std::process::Command;
+
+fn main() {
+    let output = Command::new("git")
+        .args(&["rev-parse", "--short", "HEAD"])
+        .output()
+        .unwrap();
+    let head = String::from_utf8_lossy(&output.stdout);
+    println!("cargo:rustc-env=PKG_COMMIT={}", head.trim());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
-mod configuration;
 mod command_line;
+mod configuration;
 mod session;
 
-fn main() {
-    println!("Hello, world!");
+use {
+    anyhow::{Result, bail},
+    env_logger::Env,
+    log::{error, info},
+};
+
+const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+const PKG_COMMIT: &str = env!("PKG_COMMIT");
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = try_main().await {
+        error!("{:?}", err);
+    }
+}
+
+async fn try_main() -> Result<()> {
+    // Initialize logging
+    let env = Env::default().default_filter_or(format!("{}=info", PKG_NAME.replace("-", "_")));
+    env_logger::init_from_env(env);
+
+    // Log the version
+    info!("Starting {} {} ({})", PKG_NAME, PKG_VERSION, PKG_COMMIT);
+
+    // TODO
+    bail!("Not implemented");
+
+    // Ok(())
 }
